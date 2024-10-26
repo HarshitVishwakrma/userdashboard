3ea9ff41a99f698c174baf66df8048fcb3cb2b9c
Harshit Vishwakarma <harshitvishwakarma007@gmail.com>
2024-10-26 20:27:21 +0530

    Update LeaderBoard.jsx

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -20,7 +20,7 @@ const Leaderboard = () => {
       navigate('/login')
     }
 
-    fetch('http://localhost:7000/api/user/v1/get-users')
+    fetch('https://chatappbackend-foxvyvww0-harshits-projects-548ba978.vercel.app/api/user/v1/get-users')
       .then((res) => res.json())
       .then((data) => {
         const sortedUsers = data.data.sort((a, b) => b.Points - a.Points);
